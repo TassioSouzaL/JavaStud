06506472e7ff6cadb6e4b989bb5ec42ac08ad147
Tassio <tassiolopes@outlook.com>
2026-03-30 16:19:12 -0300

    Primeiro Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int conta = 1021;
+        var Agencia = "067-8";
+        var NomeCliente = "Mario Andrade ";
+        Double Saldo = 237.48;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int Conta = sc.nextInt();
+        if (Conta == conta) {
+            System.out.println("Olá " + NomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " +
+                    Agencia + " Conta " + conta + " e seu Saldo é R$" + Saldo + " Já disponível para saque.");
+        }
     }
 }
